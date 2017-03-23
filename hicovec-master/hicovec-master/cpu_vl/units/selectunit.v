fe9a79bdfaf644232625d41daa1bccab2e8eb9aa
trivers047 <tao.ra@husky.neu.edu>
2017-03-23 12:45:37 -0400

    Update selectunit.v

--- hicovec-master/hicovec-master/cpu_vl/units/selectunit.v
+++ hicovec-master/hicovec-master/cpu_vl/units/selectunit.v
@@ -6,19 +6,18 @@
 //
 //      Description     : select a 32b output from vectorized output based on value of k_in
 //                        instead of using predefined datatype, we store 32k bit data_in in an array
-//
+//      Validity        : Modelsim proven
 //
 //--------------------------------------------------------------------------------------------
 
 `include "./programmable_const.v"
 
 module selectunit(data_in, k_in, data_out);
-   input [31:0] data_in [0:(`k-1)];
+   input [32*(`k)-1:0] data_in; //verilog doesn't allow I/O port to be array
    input [31:0] k_in;
    output [31:0] data_out;
-   integer index;
    
-   assign index = k_in;
-   assign data_out = (index<(`k)) ? data_in[index] : data_in[0];
+   
+   assign data_out = (k_in<(`k)) ? data_in[32*k_in+31 -: 32] : data_in[31:0];
    
 endmodule
